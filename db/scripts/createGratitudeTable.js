2300b283730c43b282784c7b5e3fc8567ba793da
Steven Cheung <stevencheung@hotmail.co.uk>
2022-08-08 12:09:45 +0100

    added createTable for gratitude

--- db/scripts/createGratitudeTable.js
+++ db/scripts/createGratitudeTable.js
@@ -1,14 +1,8 @@
-import { query } from "../index.js";
-//not being used
-//this is to create a gratitudetable with user and password on the registration feature
-//WILL NEED EDITING AS REQUIRED
-const sqlQuery = `CREATE TABLE IF NOT EXISTS gratitudetable(
+import { pool } from "../index.js";
+
+pool.query(`CREATE TABLE IF NOT EXISTS gratitude (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    username TEXT,
-    password TEXT)`;
-async function createGratitudeTable() {
-  console.log("Hello");
-  const res = await query(sqlQuery);
-  console.log("Table was created");
-}
-createTable();
+    gratitude TEXT,
+    date DATE NOT NULL,
+    userId INT
+ );`);
