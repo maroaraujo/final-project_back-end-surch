23a7327c503012fb92357696689a93e5b0b87d45
Steven Cheung <stevencheung@hotmail.co.uk>
2022-08-09 10:19:08 +0100

    changed index.js in db folder in order to use DATABASE_URL

--- db/index.js
+++ db/index.js
@@ -1,12 +1,20 @@
 import pg from "pg";
+import { db } from "../config/index.js";
 
 export const pool = new pg.Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: db.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
+
+// export const pool = new pg.Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
