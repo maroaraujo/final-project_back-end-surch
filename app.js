16eed57a3ddeeb3d0e85e707a749b9130acdbf34
Steven Cheung <stevencheung@hotmail.co.uk>
2022-08-09 10:24:55 +0100

    cleanedup code

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ import moodRouter from "./routes/moodRouter.js";
 
 const app = express();
 
-// const PORT = process.env.PORT || 5000;
-
 // CORS is used to connect the information from backend to frontend
 app.use(cors());
 
@@ -20,13 +18,3 @@ app.use("/gratitude", gratitudeRouter);
 app.use("/mood", moodRouter);
 
 export default app;
-
-// app.get("/", (req, res) => {
-//   res.json({ success: "true", payload: "test" });
-// });
-
-//app.listen(PORT, () => console.log(`listening ${PORT}`));
-// it is the same as bellow
-// app.listen(PORT, function () {
-//   //console.log(`listening ${PORT}`);
-// });
--- db/index.js
+++ db/index.js
@@ -7,14 +7,3 @@ export const pool = new pg.Pool({
     rejectUnauthorized: false,
   },
 });
-
-// export const pool = new pg.Pool({
-//   user: process.env.PGUSER,
-//   host: process.env.PGHOST,
-//   database: process.env.PGDATABASE,
-//   password: process.env.PGPASSWORD,
-//   port: process.env.PGPORT,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
