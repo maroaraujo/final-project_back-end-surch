b5c8175664a3d2fc2b587dfe544f9b229e86b2c9
Steven Cheung <stevencheung@hotmail.co.uk>
2022-08-10 10:57:04 +0100

    fixed typo

--- routes/achievementRouter.js
+++ routes/achievementRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllAchievement, updateAchievement } from "../models/achievement";
+import { getAllAchievement, updateAchievement } from "../models/achievement.js";
 
 const achievementRouter = express.Router();
 
